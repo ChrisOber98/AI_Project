3c5d05f5f3bd17d799dacb34d5ef9904d704b0fd
ChrisOber98 <ChrisOber98@gmail.com>
2023-04-10 15:16:01 -0400

    Fixed error
    Signed-off-by: ChrisOber98 <ChrisOber98@gmail.com>

--- main.cpp
+++ main.cpp
@@ -88,7 +88,6 @@ int main(){
 
 void Snake::displayBoard()
 {
-	cout << endl;
 	for (int i = 0; i < 12; i++)
 	{
 		for (int j = 0; j < 12; j++)
@@ -124,14 +123,8 @@ void Snake::play()
 		std::this_thread::sleep_for(std::chrono::milliseconds(250));
 		fseek(stdin,0,SEEK_END);
 
-<<<<<<< HEAD
 		//getInput();
 		exit = true;
-=======
-		if(!exit){
-			getInput();
-		}
->>>>>>> d7306acc63ef019e1fcbce569a77200dcfea45de
 	}
 	cout << "Points scored: " << points << endl;
 }
@@ -144,7 +137,7 @@ void Snake::getInput()
 	left = false;
 	down = false;
 	right = false;
-	//exit = false;
+	exit = false;
 
 	// Set terminal to raw mode 
 	system("stty raw"); 
@@ -156,30 +149,18 @@ void Snake::getInput()
 	if (input == 'w')
 	{
 		up = true;
-		if(board[snake_x - 1][snake_y] > 1 || board[snake_x - 1][snake_y] == -1){
-			exit = true;
-		}
 	}
 	else if (input == 'a')
 	{
 		left = true;
-		if(board[snake_x][snake_y - 1] > 1 || board[snake_x][snake_y - 1] == -1){
-			exit = true;
-		}
 	}
 	else if (input == 's')
 	{
 		down = true;
-		if(board[snake_x + 1][snake_y] > 1 || board[snake_x + 1][snake_y] == -1){
-			exit = true;
-		}
 	}
 	else if (input == 'd')
 	{
 		right = true;
-		if(board[snake_x][snake_y + 1] > 1 || board[snake_x][snake_y + 1] == -1){
-			exit = true;
-		}
 	}	
 	else if (input == 'x')
 	{
@@ -210,30 +191,27 @@ void Snake::updatePlayer()
 		// move the snake
 		snake_x -= 1;
 		// make sure we're not going out of bounds
-		if(snake_x == 0){
+		if(snake_x == 0)
 			snake_x += 1;
-		}
 	}
 	else if (left == true)
 	{
 		snake_y -= 1;
-		if(snake_y == 0){
+		if(snake_y == 0)
 			snake_y += 1;
-		}
 	}
 	else if (down == true)
 	{
 		snake_x += 1;
-		if(snake_x == 11){
+		if(snake_x == 11)
 			snake_x -= 1;
-		}
 	}
 	else if (right == true)
 	{
 		snake_y += 1;
-		if(snake_y == 11){
+		if(snake_y == 11)
 			snake_y -= 1;
-		}
+
 	}
 
 	// record if we are eating an apple
